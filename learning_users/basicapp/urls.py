8c7356653c8c61019c01cacc596d8bb6e098f429
max-mayer-dev <76609151+max-mayer-dev@users.noreply.github.com>
2021-02-11 15:58:39 +0100

    'first'

---
+++ learning_users/basicapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from basicapp import views
+
+# TEMPLATE TAGGING
+
+app_name = "basicapp"
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", views.user_login, name="user_login"),
+
+]
